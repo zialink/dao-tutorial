391d4ef3e35a882c62d77e3e5fee04e9265d62b4
zia <zedlinkus@gmail.com>
2022-04-20 09:47:13 +0100

    Wrote the deploy script

--- hardhat-tutorial/contracts/CryptoDevsDAO.sol
+++ hardhat-tutorial/contracts/CryptoDevsDAO.sol
@@ -6,21 +6,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 // We will add the Interfaces here
 
 contract CryptoDevsDAO is Ownable {
-    // Interfaces for FakeNFTMarketplace
-    interface IFakeNFTMarketplace {
-        function getPrice() external view returns (uint256);
 
-        function available(uint256 _tokenId) external view returns(bool);
-
-        function purchase(uint256 _tokenId) external payable;
-    }
-
-    interface ICryptoDevsNFT {
-        function balanceOf(address owner) external view returns (uint256);
-
-        function tokenOfOwnerByIndex(address owner, uint256 index) external view returns (uint256);
-    }
-    
     struct Proposal {
         uint256 nftTokenId;
         uint256 deadline;
@@ -32,6 +18,24 @@ contract CryptoDevsDAO is Ownable {
 
     mapping(uint256 => Proposal) public proposals;
     uint256 public numProposals;
+
+    // Interfaces for FakeNFTMarketplace
+    interface IFakeNFTMarketplace {
+        function getPrice() external view returns (uint256);
+
+        function available(uint256 _tokenId) external view returns(bool);
+
+        function purchase(uint256 _tokenId) external payable;
+    }
+
+
+    interface ICryptoDevsNFT {
+        function balanceOf(address owner) external view returns (uint256);
+
+        function tokenOfOwnerByIndex(address owner, uint256 index) external view returns (uint256);
+    }
+    
+    
 
     IFakeNFTMarketplace nftMarketplace;
     ICryptoDevsNFT cryptoDevsNFT;
--- hardhat-tutorial/hardhat.config.js
+++ hardhat-tutorial/hardhat.config.js
@@ -1,5 +1,9 @@
 require("@nomiclabs/hardhat-waffle");
+require("dotenv").config({ path: "../.env" });
 
+const ALCHEMY_API_KEY_URL = process.env.ALCHEMY_API_KEY_URL;
+
+const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY;
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
@@ -18,4 +22,10 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: ALCHEMY_API_KEY_URL,
+      accounts: [RINKEBY_PRIVATE_KEY],
+    },
+  },
 };
